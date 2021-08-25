1b87e26a36a67e47fec63771656fa2e1e6bf128b
Victorlouisdg <victorlouisdg@gmail.com>
2021-08-25 13:55:39 +0200

    added sleeve model

--- examples/sleeve_fold.py
+++ examples/sleeve_fold.py
@@ -1,19 +1,24 @@
-from jax import grad
-import jax.numpy as jnp
-
+import bpy
 from jax_cloth import utils
 
-def f(x):
-  y = jnp.sin(x)
-  def g(z):
-    return jnp.cos(z) * jnp.tan(y.sum()) * jnp.tanh(x).sum()
-  return grad(lambda w: jnp.sum(g(w)))(x)
+#from jax import grad
+import jax.numpy as jnp
+
+#from jax_cloth import utils
+
+#def f(x):
+#  y = jnp.sin(x)
+#  def g(z):
+#    return jnp.cos(z) * jnp.tan(y.sum()) * jnp.tanh(x).sum()
+#  return grad(lambda w: jnp.sum(g(w)))(x)
 
 
-x = 1.0
+#x = 1.0
 
-print(f(x))
+#print(f(x))
 
-print(grad(lambda x: jnp.sum(f(x)))(x))
+#print(grad(lambda x: jnp.sum(f(x)))(x))
 
-utils.test_util()
+#utils.test_util()
+
+positions, triangles = utils.get_mesh('Shirt')
--- utils.py
+++ utils.py
@@ -1,2 +1,16 @@
-def test_util():
-    print("util tested")
+import bpy
+
+def select_only(blender_object):
+    """Selects and actives a Blender object and deselects all others"""
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.context.view_layer.objects.active = blender_object
+    blender_object.select_set(True)
+
+
+def get_mesh(object_name):
+    ob = bpy.data.objects[object_name]
+    mesh = ob.data
+    positions = [(ob.matrix_world @ v.co) for v in mesh.vertices]
+    mesh.calc_loop_triangles()
+    triangles = [tri.vertices for tri in mesh.loop_triangles]
+    return positions, triangles
