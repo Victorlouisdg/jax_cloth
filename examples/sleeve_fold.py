f4af78237d4417436c9cf8dee1fa62a7d7d58b72
Victorlouisdg <victorlouisdg@gmail.com>
2021-09-09 12:18:27 +0200

    Trying out animating the mesh

--- examples/sleeve_fold.py
+++ examples/sleeve_fold.py
@@ -1,24 +1,148 @@
-import bpy
 from jax_cloth import utils, sim
-
-#from jax import grad
+from functools import partial
 import jax.numpy as jnp
+from jax import grad, vmap
+from jax.ops import index
 
-#from jax_cloth import utils
+object_name = "Shirt"
 
-#def f(x):
-#  y = jnp.sin(x)
-#  def g(z):
-#    return jnp.cos(z) * jnp.tan(y.sum()) * jnp.tanh(x).sum()
-#  return grad(lambda w: jnp.sum(g(w)))(x)
+positions, triangles = utils.get_mesh(object_name)
+positions = jnp.array(positions)
+triangles = jnp.array(triangles)
+
+positions_uv = positions[:, :2]
 
 
-#x = 1.0
+velocities = jnp.zeros_like(positions)
+initial_state = positions, velocities
 
-#print(f(x))
+# from sim import triangle_stretch_energy
 
-#print(grad(lambda x: jnp.sum(f(x)))(x))
+energy_fn = partial(sim.triangle_stretch_energy, ku=10000.0, kv=10000.0)
 
-#utils.test_util()
+# TODO think about how I can simplify this.
+def mesh_energy(positions_flat, positions_uv, triangles, energy_fn):
+    positions = positions_flat.reshape(-1, 3)
+    energies = vmap(energy_fn)(positions[triangles], positions_uv[triangles])
+    total_energy = jnp.sum(energies)
+    return -total_energy
 
-positions, triangles = utils.get_mesh('Shirt')
+
+mesh_energy_fn = partial(
+    mesh_energy, positions_uv=positions_uv, triangles=triangles, energy_fn=energy_fn
+)
+
+forces_fn = grad(mesh_energy_fn)
+
+amount_of_vertices = positions_uv.shape[0]
+system_size = 3 * amount_of_vertices
+
+m = 1.0 / amount_of_vertices
+masses = jnp.full(system_size, m)
+
+g = [0.0, 0.0, -9.81] * amount_of_vertices
+gravity = masses * jnp.array(g)
+
+M = jnp.diag(masses)
+
+forces_fn_grav = lambda x: forces_fn(x) + gravity
+
+dt = 0.01
+
+build_fn = partial(sim.build_system_BW, forces_fn=forces_fn_grav, M=M)
+
+S = jnp.identity(system_size)
+
+pinned = jnp.array([0, 4])
+
+for i in pinned:
+    S = S.at[index[3 * i : 3 * i + 3, 3 * i : 3 * i + 3]].set(0.0)
+
+z = jnp.zeros(system_size)
+
+
+step_fn = partial(sim.step_PPCG, build_fn=build_fn, S=S, z=z, dt=dt)
+
+
+def step_dummy(carry, input):
+    positions, velocities = carry
+
+    positions_new = positions.at[:, 2].add(-0.01)
+
+    carry = (positions_new, velocities)
+    output = positions_new
+    return (carry, output)
+
+
+history = sim.simulate(step_dummy, initial_state, 1000)
+
+import bpy
+
+# bpy.context.scene.frame_set(0)
+# for i in range(99):
+#     bpy.context.scene.frame_set(bpy.context.scene.frame_current + 1)
+#     ob = bpy.data.objects[object_name]
+#     mesh = ob.data
+#     for j, v in enumerate(mesh.vertices):
+#         v.co = history[i, j, :]
+
+ob = bpy.data.objects[object_name]
+mesh = ob.data
+
+sk_basis = ob.shape_key_add(name="Basis")
+
+import time
+import bmesh
+
+t0 = time.time()
+
+sk = ob.shape_key_add(name="Deform")
+
+for j, v in enumerate(mesh.vertices):
+    v.co.x += 0.2
+
+# bpy.ops.object.mode_set(mode="EDIT")
+# bm = bmesh.from_edit_mesh(mesh)
+# for j, v in enumerate(bm.verts):
+#     v.co.x += 0.2
+# bmesh.update_edit_mesh(mesh, True)
+# bpy.ops.object.mode_set(mode="OBJECT")
+
+
+# bm = bmesh.new()
+# bm.from_mesh(mesh)
+
+# print(bm.verts)
+
+# for i in range(10):
+#     # Method 1: key each vertex
+#     # for j, v in enumerate(mesh.vertices):
+#     #     v.co = history[i, j, :]
+#     #     v.keyframe_insert("co", frame=i)
+
+#     # Method 2: add shapekeys
+#     # sk = ob.shape_key_add(name="Deform")
+#     # for j in range(len(mesh.vertices)):
+#     #     sk.data[j].co = history[i, j, :]
+
+#     # Method 3: edit bmesh
+#     sk = ob.shape_key_add(name="Deform")
+
+#     for j, v in enumerate(bm.verts):
+#         v.co = history[i, j, :]
+#         # print(v.co)
+
+#     # print(type(sk.data[0]))
+#     bm.to_mesh(mesh)
+
+#     # sk.keyframe_insert(data_path="value", frame=i)
+
+#     # Does not work: keyframe all vertices at once
+#     # mesh.keyframe_insert("vertices", frame=i)
+
+
+t1 = time.time()
+
+print(t1 - t0, "seconds elapsed")
+
+print(history.shape)
---
+++ sim.py
@@ -0,0 +1,97 @@
+import jax.numpy as jnp
+from jax import grad, jacobian, vmap
+from jax.lax import scan
+from jax.ops import index
+import jax.scipy.sparse
+
+
+def deformation_gradient(positions, positions_uv):
+    u0, u1, u2 = positions_uv
+    Dm = jnp.column_stack([u1 - u0, u2 - u0])
+    Dm_inv = jnp.linalg.inv(Dm)
+
+    x0, x1, x2 = positions
+    Ds = jnp.column_stack([x1 - x0, x2 - x0])
+
+    F = Ds @ Dm_inv
+    return F
+
+
+def area(triangle_vertices):
+    v0, v1, v2 = triangle_vertices
+    return jnp.linalg.norm(jnp.cross(v1 - v0, v2 - v0)) / 2.0
+
+
+def triangle_stretch_energy(positions, positions_uv, ku, kv):
+    a = area(positions_uv)
+
+    F = deformation_gradient(positions, positions_uv)
+    wu, wv = jnp.hsplit(F, 2)
+    Cu = jnp.linalg.norm(wu) - 1
+    Cv = jnp.linalg.norm(wv) - 1
+
+    Eu = 0.5 * a * ku * (Cu ** 2)
+    Ev = 0.5 * a * kv * (Cv ** 2)
+    return Eu + Ev
+
+
+def mesh_energy(positions_flat, positions_uv, triangles, energy_fn):
+    positions = positions_flat.reshape(-1, 3)
+    energies = vmap(energy_fn)(positions[triangles], positions_uv[triangles])
+    total_energy = jnp.sum(energies)
+    return -total_energy
+
+
+def simulate(step_fn, initial_state, amount_of_steps):
+    carry, outputs = scan(step_fn, initial_state, xs=None, length=amount_of_steps)
+    return outputs
+
+
+def build_system_BW(positions, velocities, dt, forces_fn, M):
+    h = dt
+    x0 = positions.flatten()
+    v0 = velocities.flatten()
+
+    f0 = forces_fn(x0)
+    dfdx = jacobian(forces_fn)(x0)
+
+    # Equation (16) in Baraff-Witkin.
+    A = M - (h * h) * dfdx
+    b = h * (f0 + h * (dfdx @ v0))
+
+    return A, b
+
+
+def filter_system(A, b, S, z):
+    # Prefiltering the system (see also: Dynamic Deformables section 10.1.1)
+    I = jnp.identity(A.shape[0])
+    LHS = (S @ A @ S) + I - S
+    c = b - A @ z
+    rhs = S @ c
+    return LHS, rhs
+
+
+def PPCG(A, b, S, z):
+    LHS, rhs = filter_system(A, b, S, z)
+    LHS_fn = lambda x: LHS @ x
+
+    y = jax.scipy.sparse.linalg.cg(LHS_fn, rhs)[0]
+    x = y + z
+    return x
+
+
+def step_PPCG(carry, input, build_fn, S, z, dt):
+    positions, velocities = carry
+
+    A, b = build_fn(positions, velocities, dt)
+
+    x = PPCG(A, b, S, z)
+
+    delta_v = x.reshape(-1, 3)
+
+    velocities_new = velocities + delta_v
+    positions_new = positions + velocities_new * dt
+
+    carry = (positions_new, velocities_new)
+    output = positions_new
+    return (carry, output)
