9ccf44e45716f247decbf2a0a415c7a40ec6b0a0
Victorlouisdg <victorlouisdg@gmail.com>
2021-10-01 10:40:39 +0200

    Backup before rework

--- examples/sleeve_fold.py
+++ examples/sleeve_fold.py
@@ -9,12 +9,21 @@ import bpy
 bpy.context.scene.frame_set(0)
 
 object_name = "Shirt"
+goal_object_name = "Shirt Folded"
+
 ob = bpy.data.objects[object_name]
 ob.animation_data_clear()
+
+ob_goal = bpy.data.objects[goal_object_name]
+ob_goal.animation_data_clear()
 
 positions, triangles = utils.get_mesh(object_name)
 positions = jnp.array(positions)
 triangles = jnp.array(triangles)
+
+positions_goal, _ = utils.get_mesh(goal_object_name)
+positions_goal = jnp.array(positions_goal)
+
 
 positions_uv = positions[:, :2]
 
@@ -82,7 +91,7 @@ animated = jnp.array([0, 1, 2])
 
 step_fn = jit(partial(sim.step_PPCG, build_fn=build_fn, animated=animated, dt=dt))
 
-seconds = 2
+seconds = 5
 frames = fps * seconds
 steps = frames * substeps
 
@@ -92,34 +101,62 @@ mesh = ob.data
 t0 = time.time()
 
 
-def generate_bezier(start_position):
+# def generate_bezier(start_position):
+#     p0 = start_position
+#     p3 = p0.at[0].multiply(-1.0)  # mirror over YZ-plane
+#     h = jnp.linalg.norm(p3 - p0) / 2
+#     p1 = 2 / 3 * p0 + 1 / 3 * p3 + jnp.array([0, 0, h])
+#     p2 = 1 / 3 * p0 + 2 / 3 * p3 + jnp.array([0, 0, h])
+
+#     bezier_samples = [
+#         sim.bezier_evaluate(p0, p1, p2, p3, (i + 1) / steps) for i in range(steps)
+#     ]
+
+#     # For debugging
+#     viz.bezier_from_points(p0, p1, p2, p3)
+
+#     return jnp.array(bezier_samples)
+
+
+def initialize_control_points(start_position):
     p0 = start_position
     p3 = p0.at[0].multiply(-1.0)  # mirror over YZ-plane
     h = jnp.linalg.norm(p3 - p0) / 2
     p1 = 2 / 3 * p0 + 1 / 3 * p3 + jnp.array([0, 0, h])
     p2 = 1 / 3 * p0 + 2 / 3 * p3 + jnp.array([0, 0, h])
 
-    bezier_samples = [
-        sim.bezier_evaluate(p0, p1, p2, p3, (i + 1) / steps) for i in range(steps)
-    ]
-
-    # For debugging
-    viz.bezier_from_points(p0, p1, p2, p3)
-
-    return jnp.array(bezier_samples)
+    return p0, p1, p2, p3
 
 
-trajectory = jnp.stack([generate_bezier(positions[a]) for a in animated], axis=1)
+# trajectory = jnp.stack([generate_bezier(positions[a]) for a in animated], axis=1)
 
-history = sim.simulate(step_fn, initial_state, trajectory, steps)
+# history = sim.simulate(step_fn, initial_state, trajectory, steps)
 
-for i in range(frames):
-    for j, v in enumerate(mesh.vertices):
-        v.co = history[substeps * i, j, :]
-        v.keyframe_insert("co", frame=i + 1)
+# p0, p3
 
-bpy.context.scene.frame_set(0)
-bpy.context.scene.frame_end = frames
+
+def loss(free_control_points):
+
+    p1, p2 = free_control_points
+
+    # generate trajectory
+    # sim with trajectory
+    # calculate loss
+
+    return jnp.sum(jnp.square(positions_goal - positions)) / amount_of_vertices
+
+
+# grad(loss) = grad_loss
+# optimize loss
+
+print("loss", loss(history[-1]))
+# for i in range(frames):
+#     for j, v in enumerate(mesh.vertices):
+#         v.co = history[substeps * i, j, :]
+#         v.keyframe_insert("co", frame=i + 1)
+
+# bpy.context.scene.frame_set(0)
+# bpy.context.scene.frame_end = frames
 
 # Set positions back to original
 for j, v in enumerate(mesh.vertices):
